fcd7159ca29a55177236ef45d4f85c0be50807fd
lsyzg <lucas.wiz.average@gmail.com>
2023-12-28 22:06:09 -0500

    added files

---
+++ currencyflask.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, request
+import requests, json
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])
+def home():
+    if request.method == "GET":
+        return render_template("form.html")
+    amtandtype = request.form.get("amount")
+    currtoconvert = request.form.get("currency to convert to")
+
+    mylist = list(amtandtype)
+    # basecurrencyamt = int(''.join([x for x in list(filter(None, mylist)) if isinstance(x, int)]))
+    # baseunit = (''.join([x for x in mylist if isinstance(x, str)])).upper()
+
+    baseunit = request.form.get("unit")
+    basecurrencyamt = float(amtandtype)
+
+    url = 'https://api.freecurrencyapi.com/v1/latest'
+    response = requests.get(url, params={"apikey":"fca_live_qvd2XQtgRAOIxbp9H8qIx0DEq5ZngipCkJmRWFsv", "base_currency":baseunit, "currencies":currtoconvert})
+    
+    conversionratedict = response.json()
+    conversionratedict = conversionratedict["data"]
+    conversionrate = conversionratedict[currtoconvert]
+    
+    converted = round(conversionrate * basecurrencyamt, 2)
+    return render_template('form.html', convertedcurr=converted)
+
+if __name__ == "__main__":
+    app.run()
