bc0f63791886ff6f41d3a544f597fe7efead7c7c
lsyzg <lucas.wiz.average@gmail.com>
2023-12-28 23:33:16 -0500

    added style sheet, added a list of available currencies and made currencies able to be entered in lowercase

--- currencyflask.py
+++ currencyflask.py
@@ -7,22 +7,24 @@ app = Flask(__name__)
 def home():
     if request.method == "GET":
         return render_template("form.html")
-    amtandtype = request.form.get("amount")
+    amtandtype = request.form.get("amtandtype")
     currtoconvert = request.form.get("currency to convert to")
+    currtoconvert = currtoconvert.upper()
 
     mylist = list(amtandtype)
     # basecurrencyamt = int(''.join([x for x in list(filter(None, mylist)) if isinstance(x, int)]))
     # baseunit = (''.join([x for x in mylist if isinstance(x, str)])).upper()
 
     baseunit = request.form.get("unit")
+    baseunit = baseunit.upper()
     basecurrencyamt = float(amtandtype)
 
     url = 'https://api.freecurrencyapi.com/v1/latest'
     response = requests.get(url, params={"apikey":"fca_live_qvd2XQtgRAOIxbp9H8qIx0DEq5ZngipCkJmRWFsv", "base_currency":baseunit, "currencies":currtoconvert})
     
     conversionratedict = response.json()
-    conversionratedict = conversionratedict["data"]
-    conversionrate = conversionratedict[currtoconvert]
+    currencydict = conversionratedict["data"]
+    conversionrate = currencydict[currtoconvert]
     
     converted = round(conversionrate * basecurrencyamt, 2)
     return render_template('form.html', convertedcurr=converted)
