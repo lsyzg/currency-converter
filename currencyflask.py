829bd0f28060465b5b3e3af7d3ed05f6cffa03de
lsyzg <lucas.wiz.average@gmail.com>
2024-01-01 18:55:03 -0500

    changes to all files
    created dropdowns in html and css and began work on creating graphs by getting historic data

--- currencyflask.py
+++ currencyflask.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
-import requests, json, re
+from datetime import date
+import requests, re, datetime
 
 app = Flask(__name__)
 
@@ -7,23 +8,39 @@ app = Flask(__name__)
 def home():
     if request.method == "GET":
         return render_template("form.html")
-    amtandunit = request.form.get("amtandunit")
+
+    basecurrencyamt = float(request.form.get("amt"))
+    baseunit = request.form["unit"]
     currtoconvert = request.form.get("currency to convert to")
+    histconversionlst = []
+
+    # basecurrencyamt = re.search(r'[\d.]+', amtandunit)
+    # basecurrencyamt = float(basecurrencyamt.group())
+    # baseunit = re.search(r'[a-zA-Z]+', amtandunit)
+    # baseunit = baseunit.group()
+
+    baseunit = baseunit.upper()
     currtoconvert = currtoconvert.upper()
 
-    basecurrencyamt = re.search(r'[\d.]+', amtandunit)
-    basecurrencyamt = float(basecurrencyamt.group())
-    baseunit = re.search(r'[a-zA-Z]+', amtandunit)
-    baseunit = baseunit.group()
-    baseunit = baseunit.upper()
-
-    url = 'https://api.freecurrencyapi.com/v1/latest'
-    response = requests.get(url, params={"apikey":"fca_live_qvd2XQtgRAOIxbp9H8qIx0DEq5ZngipCkJmRWFsv", "base_currency":baseunit, "currencies":currtoconvert})
+    conversionurl = 'https://api.freecurrencyapi.com/v1/latest'
+    histurl = "https://api.freecurrencyapi.com/v1/historical"
     
-    responsedict = response.json()
+    conversionresponse = requests.get(conversionurl, params={"apikey":"fca_live_qvd2XQtgRAOIxbp9H8qIx0DEq5ZngipCkJmRWFsv", "base_currency":baseunit, "currencies":currtoconvert})
+    responsedict = conversionresponse.json()
     conversionrate = responsedict["data"][currtoconvert]
-
     converted = str(round(conversionrate * basecurrencyamt, 2)) + " " + currtoconvert
+
+    num_of_dates = 5
+    start = datetime.datetime.today() - datetime.timedelta(days=1)
+    date_list = [(start.date() - datetime.timedelta(days=x*365)).strftime('%Y-%m-%d') for x in range(num_of_dates)]
+    print(date_list)
+    for date in date_list:
+        historicresponse = requests.get(histurl, params={"apikey":"fca_live_qvd2XQtgRAOIxbp9H8qIx0DEq5ZngipCkJmRWFsv", "date":date, "base_currency":baseunit, "currencies":currtoconvert})
+        historicresponsedict = historicresponse.json()
+        print(historicresponsedict)
+        histconversion = historicresponsedict["data"][date][currtoconvert]
+        histconversionlst.append((date, histconversion))
+    print(histconversionlst)
     return render_template('form.html', convertedcurr=converted)
 
 if __name__ == "__main__":
